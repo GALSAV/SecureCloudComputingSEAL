a7190520c1c4d9d7387b01a5db0baaa8b9bb50d5
Savranevski Gal <gal.sav@gmail.com>
2020-03-14 22:06:03 +0200

    refactor SVNSecured to SVMSecured

--- IrisSVMSecured/Properties/Resources.Designer.cs
+++ IrisSVMSecured/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace IrisSVMSecured.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("IrisSVNSecured.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("IrisSVMSecured.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
